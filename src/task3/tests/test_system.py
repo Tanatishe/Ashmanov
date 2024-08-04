bb3dc5861df3553e2661be005bd4dc752ffcfa17
tanat <mihail-zubko@yandex.ru>
2024-08-05 01:55:56 +0300

    aiohttp

--- src/task3/tests/test_system.py
+++ src/task3/tests/test_system.py
@@ -1,6 +1,16 @@
-import requests
+import asyncio
+import aiohttp
 
 
-r = requests.get("http://127.0.0.1:8000?q=test+search")
 
-print(r.text)
+def test_request(req:str):
+    r = requests.get(f"http://127.0.0.1:8000?q={req}")
+    print(r.text)
+
+
+async def main():
+    for i in range(10):
+        task = asyncio.create_task(test_request(str(i)))
+        await task
+
+asyncio.run(main())
