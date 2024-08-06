f091da92700e4f3ca287e0d311f067ea2ac127ee
tanat <mihail-zubko@yandex.ru>
2024-08-06 11:05:00 +0300

    tests

--- src/task1/tests/test_task1.py
+++ src/task1/tests/test_task1.py
@@ -1,14 +1,20 @@
-import pytest
 from src.task1.app.main import mask_string
 
 
-
-
 def test_task1_1():
-    test_string = "Спасибо за вопрос, [Mask]ваш pin(секрет): 123-412.[/mask]. Приятных покупок!')"
-    assert mask_string(test_string) == "Спасибо за вопрос, [Mask]*** ***(******): ***-***.[/mask]. Приятных покупок!')"
+    test_string = (
+        "Спасибо за вопрос, [Mask]ваш pin(секрет): 123-412.[/mask]. Приятных покупок!')"
+    )
+    assert (
+        mask_string(test_string)
+        == "Спасибо за вопрос, [Mask]*** ***(******): ***-***.[/mask]. Приятных покупок!')"
+    )
 
 
 def test_task1_2():
     test_string = ""
     assert mask_string(test_string) == ""
+
+def test_task1_3():
+    test_string = "[mask][/mask], a[mask][/mask], [mask][/mask]b, a[mask][/mask]b, [MASK][/MASK]"
+    assert mask_string(test_string) == "[mask][/mask], a[mask][/mask], [mask][/mask]b, a[mask][/mask]b, [MASK][/MASK]"
--- src/task3/tests/test_system.py
+++ src/task3/tests/test_system.py
@@ -3,10 +3,8 @@ import asyncio
 
 
 async def main(req):
-
     async with aiohttp.ClientSession() as session:
         async with session.get(f"http://127.0.0.1:8000?q={req}") as response:
-
             print("Status:", response.status)
             print("Content-type:", response.headers["content-type"])
             html = await response.text()
